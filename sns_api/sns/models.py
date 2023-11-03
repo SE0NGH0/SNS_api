a39bb7b488669c78a465c274a40b23c14995cb13
SE0NGH0 <ericsungho@naver.com>
2023-11-03 16:05:34 +0900

    수정

--- sns_api/sns/models.py
+++ sns_api/sns/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Sns(models.Model):
-    SNSemail = models.CharField(max_length=15)
-    SNSpassword = models.CharField(max_length=15)
+    SNSemail = models.CharField(max_length=50)
+    SNSpassword = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title

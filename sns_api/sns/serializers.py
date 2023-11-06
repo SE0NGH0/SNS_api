6d897b9a97e7acb6a2a3db75f3b1116175c1015b
SE0NGH0 <140130211+SE0NGH0@users.noreply.github.com>
2023-11-06 11:20:25 +0900

    Delete sns_api directory

--- sns_api/sns/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Sns
-
-class SnsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sns
-        fields = ('id', 'SNSemail', 'SNSpassword')
--- sns_api/sns/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import SnsSerializer
-from .models import Sns
-
-# Create your views here.
-class SnsViewSet(viewsets.ModelViewSet):
-    queryset = Sns.objects.all()
-    serializer_class = SnsSerializer
